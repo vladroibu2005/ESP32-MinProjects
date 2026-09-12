462866d895d664a51678cece09652de9ddc3bde0
vladroibu2005 <101924872+vladroibu2005@users.noreply.github.com>
2026-09-12 18:57:48 +0300

    Add files via upload

---
+++ wificonnect.ino
@@ -0,0 +1,24 @@
+#include <WiFi.h>
+
+const char* ssid = "DIGI_86f4d7";
+const char* password = "21d15123";
+
+void setup(){
+    Serial.begin(115200);
+    delay(1000);
+
+    WiFi.mode(WIFI_STA); //Optional
+    WiFi.begin(ssid, password);
+    Serial.println("\nConnecting");
+
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop(){}
